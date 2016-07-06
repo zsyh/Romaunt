5da6f7e452c1b31b399c6e80fe49d5e475903568
张单旸晖 <236294386@qq.com>
2016-07-06 20:01:13 +0800

    修复使用对话框确认退出activity时的窗体泄漏bug

--- test/src/main/java/com/woofer/activity/EditNoteActivity.java
+++ test/src/main/java/com/woofer/activity/EditNoteActivity.java
@@ -486,6 +486,7 @@ public class EditNoteActivity extends Activity {
         builder.setTitle("提示");
         builder.setPositiveButton("确定", new DialogInterface.OnClickListener() {
             public void onClick(DialogInterface dialog, int which) {
+                    dialog.dismiss();
                     EditNoteActivity.this.finish();
             }
         });
