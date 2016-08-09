cdfbf8be37ca6a7355e5160d898e35d81c4b43a2
Cuise He~woof~ <657362664@qq.com>
2016-08-09 20:01:40 +0800

    更改titleba颜色和actionbar一致 在style.xml中改动几个颜色属性

--- test/src/main/java/com/woofer/activity/GithubAboutActivity.java
+++ test/src/main/java/com/woofer/activity/GithubAboutActivity.java
@@ -6,6 +6,7 @@ import android.graphics.Color;
 import android.os.Bundle;
 import android.text.ClipboardManager;
 import android.view.View;
+import android.view.Window;
 import android.widget.Button;
 import android.widget.RelativeLayout;
 import android.widget.TextView;
@@ -112,12 +113,14 @@ public class GithubAboutActivity extends Activity {
         copyurl.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
+                Toast.makeText(GithubAboutActivity.this, "链接已复制到剪贴板", Toast.LENGTH_SHORT).show();
                 ClipboardManager cm = (ClipboardManager) getSystemService(Context.CLIPBOARD_SERVICE);
                 cm.setText("https://github.com/zsyh9612/Romaunt");
-                Toast.makeText(GithubAboutActivity.this, "链接已复制到剪贴板", Toast.LENGTH_SHORT).show();
+
             }
         });
 
+        this.requestWindowFeature(Window.FEATURE_NO_TITLE);
 
         setContentView(mLayout);
 
